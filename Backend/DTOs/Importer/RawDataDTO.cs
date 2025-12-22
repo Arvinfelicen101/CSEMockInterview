375643b9be0424e5c0e67e9dc1ce355b65460d84
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2025-12-22 11:48:45 +0800

    Created dto for imported raw datas

---
+++ Backend/DTOs/Importer/RawDataDTO.cs
@@ -0,0 +1,10 @@
+namespace Backend.DTOs.Importer;
+
+public class RawDataDTO
+{
+    public string RawQuestions { get; set; }
+    public string RawCategories { get; set; }
+    public string RawSubCategories { get; set; }
+    public List<string> RawChoices { get; set; }
+    public string RawAnswers { get; set; }
+}
