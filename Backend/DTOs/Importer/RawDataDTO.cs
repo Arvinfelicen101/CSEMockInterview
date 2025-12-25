b936ecfa77ab2010be9f13ab36398097d0b4bb9d
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2025-12-25 21:58:34 +0800

    add year and period

--- Backend/DTOs/Importer/RawDataDTO.cs
+++ Backend/DTOs/Importer/RawDataDTO.cs
@@ -7,5 +7,7 @@ public class RawDataDTO
     public string RawCategories { get; set; }
     public string RawSubCategories { get; set; }
     public List<ChoiceDTO> RawChoices { get; set; }
+    public int RawYear { get; set; }
+    public string RawPeriods { get; set; }
     //year period
 }
