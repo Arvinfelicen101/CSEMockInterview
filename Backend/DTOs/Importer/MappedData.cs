34b249e2dd8752099e2f53597f6d5a95d7e52e9e
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2025-12-25 21:04:21 +0800

    commented some entities

--- Backend/DTOs/Importer/MappedData.cs
+++ Backend/DTOs/Importer/MappedData.cs
@@ -3,7 +3,7 @@ using Backend.Models;
 namespace Backend.DTOs.Importer;
 
 public record MappedData(
-    List<Category> Categories,
+    List<Category> Categories
     // List<Paragraphs> Paragraphs,
-    List<Choices> Choices
+    // List<Choices> Choices
 );
