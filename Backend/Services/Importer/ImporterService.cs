b2625afba59369aa05dad9fd47f5a344cae03c5d
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2025-12-26 22:26:14 +0800

    removed a method and add icategory service

--- Backend/Services/Importer/ImporterService.cs
+++ Backend/Services/Importer/ImporterService.cs
@@ -1,6 +1,7 @@
 using System.Text;
 using Backend.Repository.Importer;
 using Backend.DTOs.Importer;
+using Backend.Services.CategoryManagement;
 using ClosedXML.Excel;
 
 namespace Backend.Services.Importer;
@@ -8,100 +9,25 @@ namespace Backend.Services.Importer;
 public class ImporterService : IImporterService
 {
     private readonly IImporterRepository _repository;
+    private readonly ICategoryService _categoryService;
 
-    public ImporterService(IImporterRepository repository)
+    public ImporterService(IImporterRepository repository, ICategoryService categoryService)
     {
         _repository = repository;
+        _categoryService = categoryService;
     }
 
+    public async Task<IEnumerable<CategoryDTO>> getCategoryFK()
+    {
+        return await _categoryService.GetAllService();
+    }
+    
     public async Task ProcessFileAsync(ImporterDTO xlsx)
     {
-        var result = await ParseFileAsync(xlsx);
+        var categoryData = await getCategoryFK();
+        var result = await ServiceHelper.ParseFileAsync(xlsx);
         var mappeddata = await ServiceHelper.ImportMapper(result);
         
-    }
-
-    public async Task<List<RawDataDTO>> ParseFileAsync(ImporterDTO xlsx)
-    {
-        string sheetName;
-        var extractedData = new List<RawDataDTO>();
-        using (var stream = new MemoryStream())
-        {
-            var filename = xlsx.file.FileName;
-            string year = filename.Substring(0, 3);
-            int i = 5;
-            var period = new StringBuilder();
-
-            while (i < filename.Length)
-            {
-                char charPeriod = filename[i];
-
-                if (charPeriod == '_')
-                    break;
-
-                period.Append(charPeriod);
-                i++;
-            }
-
-            string result = period.ToString();
-            await xlsx.file.CopyToAsync(stream);
-            try
-            {
-                using (var workbook = new XLWorkbook(stream))
-                {
-                    foreach (var worksheet in workbook.Worksheets)
-                    {
-                        sheetName = worksheet.Name;
-                        
-                        var rows = worksheet.RowsUsed().Skip(1);
-                        foreach (var row in rows)
-                        {
-                            extractedData.Add(new RawDataDTO()
-                            {
-                                RawCategories = sheetName,
-                                RawQuestions = row.Cell(1).GetString(),
-                                RawSubCategories = row.Cell(2).GetString(),
-                                RawChoices = new List<ChoiceDTO>()
-                                {
-                                    
-                                    new ChoiceDTO()
-                                    {
-                                        ChoiceText = row.Cell(3).GetString(),
-                                        IsCorrect = false
-                                    },
-                                    new ChoiceDTO()
-                                    {
-                                        ChoiceText = row.Cell(4).GetString(),
-                                        IsCorrect = false
-                                    },
-                                    new ChoiceDTO()
-                                    {
-                                        ChoiceText = row.Cell(5).GetString(),
-                                        IsCorrect = false
-                                    },
-                                    new ChoiceDTO()
-                                    {
-                                        ChoiceText = row.Cell(6).GetString(),
-                                        IsCorrect = true
-                                    }
-                                    
-                                },
-                            
-                                RawParagraph = row.Cell(7).GetString(),
-                                RawYear = Convert.ToInt32(year),
-                                RawPeriods = result
-                            });
-                        }
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                throw new Exception();
-            }
-            
-        }
-        return extractedData;
     }
     
 }
