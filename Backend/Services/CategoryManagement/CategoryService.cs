a6fd14150a564cf1adc1493194c03abbcfd53309
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2025-12-26 20:43:09 +0800

    set cache and added logger

--- Backend/Services/CategoryManagement/CategoryService.cs
+++ Backend/Services/CategoryManagement/CategoryService.cs
@@ -22,13 +22,23 @@ public class CategoryService : ICategoryService
     public async Task<IEnumerable<CategoryDTO>> GetAllService()
     {
         // _logger.LogInformation("Fetching all categories...");
-        if (_cache.TryGetValue(CategoryCacheKey, out List<CategoryDTO> cached)) return cached;
+        if (_cache.TryGetValue(CategoryCacheKey, out List<CategoryDTO> cached))
+        {
+            _logger.LogInformation("CATEGORY CACHE HIT");
+            return cached;
+        }
+        
+        _logger.LogInformation("Category hit missed");
         var result = await _repository.GetAllAsync();
         var mapCategory = result.Select(c => new CategoryDTO()
         {
+            
             Id = c.Id,
             CategoryName = c.CategoryName.ToString()
-        });
+        }).ToList();
+        
+        //set cache
+        _cache.Set(CategoryCacheKey, mapCategory,TimeSpan.FromMinutes(10));
         return mapCategory;
     }
 }
