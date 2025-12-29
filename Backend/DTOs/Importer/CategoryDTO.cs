adff8e260c869341f5c59de5c5a24ae2892fed34
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2025-12-29 13:56:31 +0800

    refactor fields

--- Backend/DTOs/Importer/CategoryDTO.cs
+++
@@ -1,7 +0,0 @@
-namespace Backend.DTOs.Importer;
-
-public class CategoryDTO 
-{
-    public int Id { get; set; }
-    public string CategoryName { get; set; }
-}
---
+++ Backend/DTOs/Importer/YearPeriodDTO.cs
@@ -0,0 +1,8 @@
+namespace Backend.DTOs.Importer;
+
+public class YearPeriodDTO 
+{
+    public int Id { get; set; }
+    public int year { get; set; }
+    public string period { get; set; } 
+}
