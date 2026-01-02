dd18d2bb6db1bcfc5475b46fe1318053e5e1ba8d
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2026-01-02 14:26:53 +0800

    add required and optional fields

--- Backend/Models/Results.cs
+++ Backend/Models/Results.cs
@@ -3,9 +3,9 @@ namespace Backend.Models;
 public class Results
 {
     public int Id { get; set; }
-    public int TotalScore { get; set; }
-    public bool IsPassed { get; set; }
+    public required int TotalScore { get; set; }
+    public required bool IsPassed { get; set; }
     //Foreign Key
-    public string UserId { get; set; }
-    public Users UsersNavigation { get; set; }
+    public required string UserId { get; set; }
+    public Users? UsersNavigation { get; set; }
 }
