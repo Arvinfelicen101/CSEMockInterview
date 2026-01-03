b8c16b98e7010cefdadf04a3fa8e0b6351af49b9
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2026-01-03 10:58:49 +0800

    add required and null on properties

--- Backend/DTOs/Question/QuestionListDTO.cs
+++ Backend/DTOs/Question/QuestionListDTO.cs
@@ -2,8 +2,8 @@
 {
     public class QuestionListDTO
     {
-        public string QuestionName {  get; set; }
-        public int SubCategoryId { get; set; }
+        public required string QuestionName {  get; set; }
+        public required int SubCategoryId { get; set; }
         public int? ParagraphId { get; set; }
         public int YearPeriodId { get; set; }
   
--- Backend/DTOs/Question/QuestionReadDTO.cs
+++ Backend/DTOs/Question/QuestionReadDTO.cs
@@ -5,11 +5,11 @@ namespace Backend.DTOs.Question
     public class QuestionReadDTO
     {
         public int Id { get; set; }
-        public string QuestionName { get; set; }
+        public required string QuestionName { get; set; }
         public int SubCategoryId { get; set; }
         public int? ParagraphId { get; set; }
         public int YearPeriodId { get; set; }
-        public List<ChoiceReadDTO> choices { get; set; }
+        public required List<ChoiceReadDTO> choices { get; set; }
 
     }
 }
