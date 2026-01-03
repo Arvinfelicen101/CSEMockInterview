cd5774e4ed7050a9c5ae9a3bc3fc221d4218b923
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2026-01-03 10:50:45 +0800

    remove unused dto

--- Backend/DTOs/Importer/YearPeriodDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace Backend.DTOs.Importer;
-
-public class YearPeriodDTO 
-{
-    public int Id { get; set; }
-    public required int year { get; set; }
-    public required string period { get; set; } 
-}
