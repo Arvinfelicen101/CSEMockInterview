a73efd16e137225a0497c5988b8085192738f397
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2026-01-08 21:33:43 +0800

    added question cache in dto

--- Backend/DTOs/Importer/FKDataDTOs.cs
+++ Backend/DTOs/Importer/FKDataDTOs.cs
@@ -7,4 +7,5 @@ public class FKDataDTOs
     public required IEnumerable<YearPeriods> YearPeriodFK { get; set; }
     public required IEnumerable<Paragraphs> ParagraphFK { get; set; }
     public required IEnumerable<SubCategories> subCategoriesFK { get; set; }
+    public required IEnumerable<Questions> questionsCache { get; set; }
 }
